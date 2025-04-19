83f790a7ba5730bb2cf70557616a23f4aaee322b
Umaid <ahmedumaid707@gmail.com>
2025-04-19 12:50:18 +0530

    Update utils.py

--- helper/utils.py
+++ helper/utils.py
@@ -69,6 +69,14 @@ def TimeFormatter(milliseconds: int) -> str:
     )
     return tmp.rstrip(", ")
 
+def convert(seconds):
+    seconds = seconds % (24 * 3600)
+    hour = seconds // 3600
+    seconds %= 3600
+    minutes = seconds // 60
+    seconds %= 60      
+    return "%d:%02d:%02d" % (hour, minutes, seconds)
+
 async def send_log(client, user, log_message: str):
     try:
         await client.send_message(
